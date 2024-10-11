6cd51f8fdf059ba8e4e812de3fb59b36de2d0cbf
agusv <agusvarela5@gmail.com>
2024-10-10 21:17:07 -0300

    creacion del repositorio

---
+++ src/Principal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Bienvenido/a al conversor de Moneda!");
+            System.out.println("\n Seleccione la opción que desea: \n");
+            System.out.println("1) Dólar a Peso Argentino");
+            System.out.println("2) Peso Argentino a Dólar");
+            System.out.println("3) Dólar a Real Brasileño");
+            System.out.println("4) Real Brasileño a Dólar");
+            System.out.println("5) Dólar a Peso Colombiano");
+            System.out.println("6) Peso Colombiano a Dólar");
+            System.out.println("7) Salir");
+
+            int opcion = lectura.nextInt();
+
+
+        }
+    }
+}
---
+++ src/com/aluracursos/conversorapp/modelos/Conversion.java
@@ -0,0 +1,8 @@
+package com.aluracursos.conversorapp.modelos;
+
+public class Conversion {
+    public double valorAConvertir;
+    public double valorConvertido;
+
+
+}
