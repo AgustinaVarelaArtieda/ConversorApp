6bd9feadcd190b41df865de1861b69284fece3f1
agusv <agusvarela5@gmail.com>
2024-10-14 20:35:58 -0300

    Actualizacion de Principal para ejecutar el menu de opciones

--- src/Principal.java
+++ src/Principal.java
@@ -1,23 +1,12 @@
-import java.util.Scanner;
+import com.aluracursos.conversorapp.modelos.Menu;
 
 public class Principal {
     public static void main(String[] args) {
-        Scanner lectura = new Scanner(System.in);
-
-        while(true){
-            System.out.println("Bienvenido/a al conversor de Moneda!");
-            System.out.println("\n Seleccione la opción que desea: \n");
-            System.out.println("1) Dólar a Peso Argentino");
-            System.out.println("2) Peso Argentino a Dólar");
-            System.out.println("3) Dólar a Real Brasileño");
-            System.out.println("4) Real Brasileño a Dólar");
-            System.out.println("5) Dólar a Peso Colombiano");
-            System.out.println("6) Peso Colombiano a Dólar");
-            System.out.println("7) Salir");
-
-            int opcion = lectura.nextInt();
-
-
+        try {
+            Menu menu = new Menu();
+            menu.run();
+        } catch (Exception e){
+            System.err.println("Error al iniciar la aplicación: " + e.getMessage());
         }
     }
 }
